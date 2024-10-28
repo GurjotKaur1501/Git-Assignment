dbd0d726073e852ffb22d6ded0f65a4caa48b613
GurjotKaur1501 <jotkau0115@gmail.com>
2024-10-28 02:19:43 +0100

    adding main class

---
+++ GuessingGame.java
@@ -0,0 +1,9 @@
+public class GuessingGame {
+    public static void main(String[] args) {
+        // Create an instance of Guesser with the range 0 to 1000
+        Guesser guesser = new Guesser(0, 1000);
+
+        // Start the guessing game
+        guesser.start();
+    }
+}
