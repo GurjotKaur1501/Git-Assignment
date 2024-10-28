de48532496d12f8605f7df03b7810287f8edaace
GurjotKaur1501 <jotkau0115@gmail.com>
2024-10-28 02:35:57 +0100

    updating GuessingGame from 1000 to 10.

--- GuessingGame.java
+++ GuessingGame.java
@@ -1,7 +1,7 @@
 public class GuessingGame {
     public static void main(String[] args) {
         // Create an instance of Guesser with the range 0 to 1000
-        Guesser guesser = new Guesser(0, 1000);
+        Guesser guesser = new Guesser(0, 10);
 
         // Start the guessing game
         guesser.start();
