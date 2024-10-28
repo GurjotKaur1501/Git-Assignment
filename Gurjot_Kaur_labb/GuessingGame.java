4ab746c96836e8af0d6a34ee80b741617d994fb7
GurjotKaur1501 <jotkau0115@gmail.com>
2024-10-28 16:27:27 +0100

    Deleting test labb work

--- Gurjot_Kaur_labb/GuessingGame.java
+++
@@ -1,9 +0,0 @@
-public class GuessingGame {
-    public static void main(String[] args) {
-        // Create an instance of Guesser with the range 0 to 1000
-        Guesser guesser = new Guesser(0, 10);
-
-        // Start the guessing game
-        guesser.start();
-    }
-}
